f61f292941a597bbdbb859c636be211135134f61
ghilxn <ghilancollege@gmail.com>
2025-09-06 18:04:21 -0700

    Add files via upload

---
+++ CISC179/W1/first code.py
@@ -0,0 +1,33 @@
+# a. Using multiple print() statements
+print("All the world's a stage,")
+print("And all the men and women merely players:")
+print("They have their exits and their entrances;")
+print("And one man in his time plays many parts,")
+print("His acts being seven ages.")
+
+# b. Single print with
+print("\nAll the world's a stage,\nAnd all the men and women merely players:\nThey have their exits and their entrances;\nAnd one man in his time plays many parts,\nHis acts being seven ages.")
+
+# c. Output of print("127","0","0","1", sep=".") is 127.0.0.1
+
+# flush=True: makes Python show the output right away
+# print("Loading...", flush=True)
+
+# flush=False: normal printing (waits and shows when ready)
+# print("Done!", flush=False)
+
+# flush is usually off
+# but when we use end="\n" (new line),
+# print() acts like flush=True
+
+
+# One challenge I faced was getting used to PyCharm since I have never used it before.
+
+# Remembering to use \n for line breaks in a single print() statement was confusing at first.
+
+# Understanding flush=True and flush=False was tricky because I didn’t know what buffering meant.
+
+# I had to re-read the instructions a few times to make sure I was putting answers in the right format.
+
+
+
